3b14d5d762ba489eded72374072f229ed64fa28a
Keltos <c.kaya@home.nl>
2015-01-24 21:54:59 +0100

    Cached inverse matrix calculation

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
 ## Purpose of these functions are to cache potential time consuming inverse marix calculations.
-## Example x<-makeCacheMatrix(matrix(c(4,3,3,2),2,2)) with its inverse y<-matrix(c(-2,3,3,-4),2,2). 
 #--------------------------------------------------------------------------------------------------
-#get the matrix: x$get() 
-##  solve its inverse xinv<-cacheSolve(x)
-##  check its inverse is cached: x$getinv()
-## set the matrix to an arbitrary matrix for instance its inverse:  x$set(matrix(c(-2,3,3,-4),2,2))
-## set the inverse matrix: x$setinv(y)
+## Example x<-makeCacheMatrix(matrix(c(4,3,3,2),2,2)) with its inverse y<-matrix(c(-2,3,3,-4),2,2). 
+## Get the matrix: x$get() 
+## Solve its inverse xinv<-cacheSolve(x)
+## Check its inverse is cached: x$getinv()
+## Set the matrix to an arbitrary matrix for instance its inverse:  x$set(matrix(c(-2,3,3,-4),2,2))
+## Set the inverse matrix: x$setinv(y)
 #--------------------------------------------------------------------------------------------------
 
 ## creates a special matrix containg list of functions to: set/get value/inverse of a matrix
